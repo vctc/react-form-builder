4f13e5bc2c51546701d00f2b6e35985819473b83
victor <victorrajkumar1@gmail.com>
2020-07-29 14:24:37 +0530

    implemented antd design and started the home component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.scss";
+import { Layout, Menu, Row, Col, Divider } from "antd";
+import "antd/dist/antd.css";
+import Logo from "./images/logo.webp";
 
 function App() {
+  const { Header, Content, Footer } = Layout;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Layout>
+        <Header className="app__header">
+          <>
+            <div className="app__header-logo">
+              <img src={Logo} />
+            </div>
+            <Menu className="app__header__menu" theme="dark" mode="horizontal">
+              <Menu.Item>Home</Menu.Item>
+              <Menu.Item>About</Menu.Item>
+              <Menu.Item>Contact</Menu.Item>
+            </Menu>
+          </>
+        </Header>
+        <Content className="app__content">
+          <Row gutter={16}>
+            <Col span={6}>
+              <Divider
+                orientation="center"
+                style={{ color: "#333", fontWeight: "normal" }}
+              >
+                Forms
+              </Divider>
+            </Col>
+            <Col span={12}>
+              <Divider
+                orientation="center"
+                style={{ color: "#333", fontWeight: "normal" }}
+              >
+                Playground
+              </Divider>
+            </Col>
+            <Col span={6}>
+              <Divider
+                orientation="center"
+                style={{ color: "#333", fontWeight: "normal" }}
+              >
+                Form Inputs
+              </Divider>
+            </Col>
+          </Row>
+        </Content>
+        <Footer className="app__footer">Made with ❤️ by TuringTest</Footer>
+      </Layout>
     </div>
   );
 }
